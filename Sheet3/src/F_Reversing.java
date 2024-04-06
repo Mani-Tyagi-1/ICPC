e6d699b051756f7c4d098f438fdd57ec7d0b3a8e
ManiTyagi <manityagi1919@gmail.com>
2024-04-07 01:52:40 +0530

    added F_Reversing

---
+++ Sheet3/src/F_Reversing.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*;
+public class F_Reversing
+{
+    static PrintWriter out=new PrintWriter((System.out));
+    public static void main(String args[])throws IOException
+    {
+    //     Reader sc=new Reader();
+    //     int t=sc.nextInt();
+    //     while(t-->0)
+    //     {
+            solve();
+    //     }
+    // out.close();
+    }
+
+    public static void solve()
+    {
+        Reader R = new Reader();
+
+        int len = R.nextInt();
+
+        Long arr[] = new Long[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = R.nextLong();
+        }
+
+        int start = 0;
+        int end = len-1;
+
+        while (start <= end) {
+            long temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        for (int i = 0; i < len; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+    static class Reader 
+    { 
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        public String next()
+        {
+            while(!st.hasMoreTokens())
+            {
+                try
+                {
+                    st=new StringTokenizer(br.readLine());
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+        public double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        public String nextLine()
+        {
+            try
+            {
+                return br.readLine();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public boolean hasNext()
+        {
+            String next=null;
+            try
+            {
+                next=br.readLine();
+            }
+            catch(Exception e)
+            {
+            }
+            if(next==null)
+            {
+                return false;
+            }
+            st=new StringTokenizer(next);
+            return true;
+        }
+    } 
+}
