1574cad18c3f6ea696233bf5469d8b7b40202a23
ManiTyagi <manityagi1919@gmail.com>
2024-07-18 23:42:00 +0530

    Sheet8 started

---
+++ Sheet8/B_Watermelon.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+public class B_Watermelon
+{
+    static PrintWriter out=new PrintWriter((System.out));
+    public static void main(String args[])throws IOException
+    {
+    //     Reader sc=new Reader();
+    //     int t=sc.nextInt();
+    //     while(t-->0)
+    //     {
+            solve();
+    //     }
+    // out.close();
+    }
+
+    public static void solve()
+    {
+        Reader sc = new Reader();
+        
+        int n = sc.nextInt();
+
+        if (n > 2 && n % 2 == 0) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+    
+    static class Reader 
+    { 
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        public String next()
+        {
+            while(!st.hasMoreTokens())
+            {
+                try
+                {
+                    st=new StringTokenizer(br.readLine());
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+        public double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        public String nextLine()
+        {
+            try
+            {
+                return br.readLine();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public boolean hasNext()
+        {
+            String next=null;
+            try
+            {
+                next=br.readLine();
+            }
+            catch(Exception e)
+            {
+            }
+            if(next==null)
+            {
+                return false;
+            }
+            st=new StringTokenizer(next);
+            return true;
+        }
+    } 
+}
